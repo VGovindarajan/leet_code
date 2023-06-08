efe6c7c3c768ee4b713aa5610cc1ef6ac2f2acd1
Vijayarajan Govindarajan <vg2131@gmail.com>
2023-06-07 19:06:50 -0500

    Formatting changes

--- matching_engine.py
+++ matching_engine.py
@@ -4,6 +4,7 @@ from typing import List
 from queue import PriorityQueue
 import time
 
+
 # Would be of interest to know the run time of this engine
 
 
@@ -50,6 +51,7 @@ class Fill:
 
     def __eq__(self, other):
         return self.side == other.side and self.qty == other.qty and self.trader == other.trader and self.c_party == other.c_party and self.price == other.price
+
 
 class MatchingEngine:
     def __init__(self):
@@ -155,7 +157,6 @@ class MatchingEngine:
 
 
 def main():
-
     o1 = Order(100, "VG", Side.BUY, 50.00, datetime.now().timestamp())
     time.sleep(0.1)
     o4 = Order(300, "C2", Side.BUY, 50.00, datetime.now().timestamp())
@@ -174,14 +175,12 @@ def main():
     f5 = Fill(50, "C3", "C2", Side.SELL, 50.00, None)
     f6 = Fill(50, "C2", "C3", Side.BUY, 50.00, None)
 
-
     order1 = [o1, o2, o3]
     expected1 = [f1, f2]
     engine = MatchingEngine()
     actual1 = engine.process(order1)
     print(actual1)
     assert expected1 == actual1
-
 
     order2 = [o1, o2, o3, o4]
     expected2 = [f1, f2, f3, f4]
@@ -190,13 +189,13 @@ def main():
     print(actual2)
     assert expected2 == actual2
 
-
     order3 = [o1, o2, o3, o4, o5]
     expected3 = [f1, f2, f3, f4, f5, f6]
     engine = MatchingEngine()
     actual3 = engine.process(order3)
     print(actual3)
     assert expected3 == actual3
+
 
 if __name__ == "__main__":
     main()
